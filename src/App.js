5236e072dc812cc70bb9fb52f8912f36e2f70823
Shoaib-78637 <syedshoaibabbas786@gmail.com>
2020-06-14 16:22:25 +0500

    counter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  let [count,setcount]=useState(1);
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="night"> 
+      <div className='box'>
+      <h2>Increment & Decrement Operation</h2>
+      <form>
+        <input type="number" value={count}></input>
+       
+      </form>
+      <button onClick={()=> setcount(count=count+1)
+      } >Increment</button>
+      
+      </div>
+
     </div>
   );
 }
